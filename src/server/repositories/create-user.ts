e1b18ed80aff229a479b2dc4802309f5432b7088
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-16 11:42:40 -0300

    feat(create-user): adding repository

---
+++ src/server/repositories/create-user.ts
@@ -0,0 +1,18 @@
+import { PrismaClient, User } from "@prisma/client";
+import { UserDTO } from "../interface/interfaceDTO";
+import { ICreateUserRepository } from "./protocols";
+import { Not_Fould } from "../errors/api-errors";
+
+export class PostgresCreateUser implements ICreateUserRepository {
+  private User = new PrismaClient().user;
+
+  async create(params: UserDTO): Promise<User> {
+    const user = await this.User.create({ data: params });
+
+    if (!user) {
+      throw new Not_Fould("Error no banco ao tentar criar o usuario!");
+    }
+
+    return user;
+  }
+}
---
+++ src/server/repositories/protocols.ts
@@ -0,0 +1,6 @@
+import { User } from "@prisma/client";
+import { UserDTO } from "../interface/interfaceDTO";
+
+export interface ICreateUserRepository {
+  create(params: UserDTO): Promise<User>;
+}
