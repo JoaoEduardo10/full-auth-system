2fba86a44e390493a7d6fdeee923819e44fbf937
João Eduardo <joaoeduardoj920@gmail.com>
2023-08-03 18:21:48 -0300

    refactory(create-user): ajust repository

--- src/server/repositories/create-user.ts
+++ src/server/repositories/create-user.ts
@@ -2,9 +2,10 @@ import { PrismaClient, User } from "@prisma/client";
 import { UserDTO } from "../interface/interfaceDTO";
 import { ICreateUserRepository } from "./protocols";
 import { Not_Fould } from "../errors/api-errors";
+import { prisma } from "../../database/prisma";
 
 export class PrismaCreateUserRepository implements ICreateUserRepository {
-  private User = new PrismaClient().user;
+  private User = prisma.user;
 
   async create(params: UserDTO): Promise<User> {
     const user = await this.User.create({ data: params });
